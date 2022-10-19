4a1079e916fb30a898d36f9800ae72ba887f5318
KhangPhamThe <khang.pham271200@hcmut.edu.vn>
2022-10-19 11:53:44 +0700

    setup scss and nextUI

---
+++ smart-door/pages/user/registerPage.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <p>Register Page nha</p>
+            
+            <Link href="/user/loginPage">Link Qua trang cua Login nha</Link>
+        </div>
+    )
+}
+
+export default RegisterPage
