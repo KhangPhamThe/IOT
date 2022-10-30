d3c14811dfb59e580416a6dde34fe9c634326b4c
KhangPhamThe <khang.pham271200@hcmut.edu.vn>
2022-10-30 16:57:28 +0700

    save progress

---
+++ smart-door/assets/constants/color.ts
@@ -0,0 +1,11 @@
+export const nextUIColor = {
+    WHITE: 'white',
+    WARNING: 'warning',
+    SUCCESS: 'success',
+    ERROR: 'error',
+    PRIMARY: 'primary',
+    SECONDARY: 'secondary',
+    GRADIENT: 'gradient',
+}
+
+export const colorList = [nextUIColor.SUCCESS, nextUIColor.WARNING, nextUIColor.ERROR, nextUIColor.PRIMARY, nextUIColor.SECONDARY, nextUIColor.WHITE, nextUIColor.GRADIENT];
