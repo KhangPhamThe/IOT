040ac5d37af18e1cef9c12efb51e0351fc377d6a
InuYasha0990 <hiamsecondgosu@gmail.com>
2022-12-01 09:04:05 +0700

    logic for login

--- smart-door-be/controllers/users.js
+++ smart-door-be/controllers/users.js
@@ -47,11 +47,10 @@ export const createAccount = async (req, res) => {
   }
 };
 
-
 // Todo: Permission for admin
 export const getAllUsers = async (req, res) => {
   try {
-    const users = await UserModel.find({});
+    const users = await UserModel.find({ role: "user" });
     res.send(users);
   } catch (err) {
     res.status(500).json({ err: err });
@@ -79,7 +78,36 @@ export const loginAccount = async (req, res) => {
     const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
       expiresIn: 60 * 60 * 24,
     });
-    res.header("auth-token", token).send(token);
+
+    res.header("auth-token", token).send({
+      tokenType: "jwt",
+      token,
+      expireTime: new Date(Date.now() + 60 * 60 * 24 * 1000).toUTCString(),
+      email: user?.email,
+      firstName: user?.firstName,
+      lastName: user?.lastName,
+      avatarURL: user?.avatarURL,
+      role: user?.role,
+    });
+  } catch (err) {
+    res.status(500).json({ err: err });
+  }
+};
+
+export const getUserProfile = async (req, res) => {
+  // Authenticate
+  try {
+    const token = req.header("auth-token");
+    const jwtDecode = jwt.decode(token);
+
+    const user = await UserModel.findOne({ _id: jwtDecode["_id"] });
+    res.send({
+      email: user?.email,
+      firstName: user?.firstName,
+      lastName: user?.lastName,
+      avatarURL: user?.avatarURL,
+      role: user?.role,
+    });
   } catch (err) {
     res.status(500).json({ err: err });
   }
--- smart-door-be/routers/users.js
+++ smart-door-be/routers/users.js
@@ -3,13 +3,15 @@ import {
   createAccount,
   loginAccount,
   getAllUsers,
+  getUserProfile,
 } from "../controllers/users.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/admin/create-account", createAccount);
+router.post("/admin/create-account", verifyToken ,createAccount);
 router.get("/admin/all-users", verifyToken, getAllUsers);
 router.post("/login", loginAccount);
+router.get('/get-user-profile', verifyToken ,getUserProfile);
 
 export default router;
