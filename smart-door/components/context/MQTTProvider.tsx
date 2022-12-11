4134d67ffaadef1937ee4b526706eedd187f88aa
InuYasha0990 <hiamsecondgosu@gmail.com>
2022-12-11 07:22:58 +0700

    hello

--- smart-door/components/context/MQTTProvider.tsx
+++ smart-door/components/context/MQTTProvider.tsx
@@ -10,9 +10,9 @@ const client = mqtt.connect(process.env.NEXT_PUBLIC_MQTT_URL || "", {
     host: process.env.NEXT_PUBLIC_MQTT_URL || "",
     clientId: `mqtt_234567`,
     keepalive: 60,
-    reconnectPeriod: 20000,
-    // protocolId: 'MQIsdp',
-    // protocolVersion: 3,
+    // reconnectPeriod: 1000,
+    protocolId: 'MQIsdp',
+    protocolVersion: 3,
     username: "KhangPhamThe",
     password: "headingtonz",
     protocol: "mqtts",
@@ -24,6 +24,8 @@ const topics = [
     "KhangPhamThe/feeds/dadn.ppl-out",
     "KhangPhamThe/feeds/dadn.ppl-in",
     "KhangPhamThe/feeds/dadn.led",
+    "KhangPhamThe/feeds/dadn.allow",
+    "KhangPhamThe/feeds/dadn.light",
 ]
 
 const MQTTContext = createContext({});
@@ -32,16 +34,18 @@ export const MQTTProvider = (props: Props) => {
     const currUserSelection = useAppSelector(state => state.user);
     
     useEffect(() => {
-        client.on('connect', () => {
-            console.log('connected to adafruit')
-            client.subscribe(topics, () => {
-                console.log("subscribed to all board")
-            })
-        });
-        client.on('message', (topic:any, payload:any) => {
-            console.log("received message", topic, payload.toString())
-        });
-    }, [])
+        if (currUserSelection) {
+            client.on('connect', () => {
+                console.log('connected to adafruit')
+                client.subscribe(topics, () => {
+                    console.log("subscribed to all board")
+                })
+            });
+            client.on('message', (topic:any, payload:any) => {
+                console.log("received message", topic, payload.toString())
+            });
+        }
+    }, [currUserSelection])
     
     return (
         <MQTTContext.Provider value={{}}>
