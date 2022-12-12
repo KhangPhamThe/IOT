6bec0bce151a9dd0e3a487ba909a055659257c60
KhangPhamThe <khang.pham271200@hcmut.edu.vn>
2022-12-12 21:18:03 +0700

    update routing between user and admin

--- smart-door/components/home/leftBar/categoryItem.tsx
+++ smart-door/components/home/leftBar/categoryItem.tsx
@@ -1,22 +1,36 @@
 import styles from 'styles/leftBar.module.scss';
 import generalStyles from 'styles/general.module.scss';
 import { useMemo } from 'react';
+import Link from 'next/link';
 
 interface CategoryItemProps {
     icon: React.ReactNode;
     title: string;
     active?: boolean;
     unavailable?: boolean;
+    url?: string;
     style?: React.CSSProperties;
 }
 
-const CategoryItem = ({icon, title, style, active=false, unavailable}:CategoryItemProps) => {
+const CategoryItem = ({ icon, title, style, active = false, unavailable, url }: CategoryItemProps) => {
     const finalClassName = useMemo(() => {
         let returnedClass = styles.categoryItem;
         if (active) returnedClass += ` ${generalStyles.whiteText}`;
         if (unavailable) returnedClass += ` ${generalStyles.blockCursor}`;
         return returnedClass;
-    },[active, unavailable]);
+    }, [active, unavailable]);
+
+    if (url) return (
+        <Link href={url}>
+            <div className={finalClassName} style={style}>
+                {icon}
+                <h3>{title}</h3>
+
+                {/* Active cursor */}
+                {active && <div className={styles.activeCursor}></div>}
+            </div>
+        </Link>
+    )
 
     return (
         <div className={finalClassName} style={style}>
