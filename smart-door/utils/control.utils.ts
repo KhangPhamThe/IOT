0531c91da91c801a61011a010acf023e6e6a2b76
KhangPhamThe <khang.pham271200@hcmut.edu.vn>
2023-01-04 18:48:21 +0700

    add keyword

--- smart-door/utils/control.utils.ts
+++ smart-door/utils/control.utils.ts
@@ -1,5 +1,5 @@
 export const evaluateLogImportance = (text:string) => {
-    if (text.search(/break/) > -1 || text.search(/two people/) > -1 || text.search(/2 people/) > -1 || text.search(/missing/) > -1) {
+    if (text.search(/broken/) > -1 || text.search(/break/) > -1 || text.search(/two people/) > -1 || text.search(/2 people/) > -1 || text.search(/missing/) > -1) {
         return 'high';
     }
     else if (text.search(/late/) > -1) {
